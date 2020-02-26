b9f38f5b24d4186e66b75953aae5cb08e7d97598
thang241190 <thangnguyen24111990@gmail.com>
2020-02-26 20:43:02 +0200

    Updates

---
+++ precache-manifest.8d31b515381c52bb179cae2706b39d71.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd13a2fb2db02bbd512aae240c62489c",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "144cad63306b6bd2d1e3",
+    "url": "/weather/static/css/main.bcebed1f.chunk.css"
+  },
+  {
+    "revision": "0bb18ebe95e6f90c49fc",
+    "url": "/weather/static/js/2.8599a5db.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/weather/static/js/2.8599a5db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "144cad63306b6bd2d1e3",
+    "url": "/weather/static/js/main.5e77613f.chunk.js"
+  },
+  {
+    "revision": "02f80e29b5552fb91c48",
+    "url": "/weather/static/js/runtime-main.f075ed78.js"
+  }
+]);

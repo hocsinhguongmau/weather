608f5f4862fd09f72c9044050a1ed1d78934cdb0
thang241190 <thangnguyen24111990@gmail.com>
2021-05-12 11:09:49 +0300

    Fix cors

--- src/components/Weather/Weathers.js
+++ src/components/Weather/Weathers.js
@@ -1,13 +1,13 @@
-import React, {Component} from "react";
-import Autocomplete from "react-google-autocomplete";
-import Geocode from "react-geocode";
-import CurrentWeather from "./CurrentWeather";
-import HourlyWeather from "./HourlyWeather";
+import React, { Component } from "react"
+import Autocomplete from "react-google-autocomplete"
+import Geocode from "react-geocode"
+import CurrentWeather from "./CurrentWeather"
+import HourlyWeather from "./HourlyWeather"
 
 const api = {
 	key: "e985b57d27d16a6af5f72b816e5afe31",
-	base: "https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast"
-};
+	base: "http://thangnguyen24111990-eval-test.apigee.net/weather/forecast",
+}
 
 class Weathers extends Component {
 	state = {
@@ -18,82 +18,86 @@ class Weathers extends Component {
 		currentWeather: {},
 		searchStatus: false,
 		newCoordinates: {},
-		hourlyWeather: {}
-	};
+		hourlyWeather: {},
+	}
 	fetchLocation(lat, lon) {
 		fetch(`${api.base}/${api.key}/${lat},${lon}`)
-			.then(res => res.json())
+			.then((res) => res.json())
 			.then(
-				result => {
+				(result) => {
 					this.setState({
 						isLoaded: true,
 						currentWeather: result.currently,
 						hourlyWeather: result.hourly,
-						searchStatus: true
-					});
+						searchStatus: true,
+					})
 				},
-				error => {
+				(error) => {
 					this.setState({
 						isLoaded: true,
 						searchStatus: "Searching...",
-						error
-					});
-				}
+						error,
+					})
+				},
 			)
-			.then(this.getLocationName(lat, lon));
+			.then(this.getLocationName(lat, lon))
 	}
 
 	getLocationName(lat, lon) {
-		Geocode.setApiKey("AIzaSyCO9x6JVamwo9b09PmJkpkUrYR3FI9mK6A");
-		Geocode.setLanguage("en");
+		Geocode.setApiKey("AIzaSyCO9x6JVamwo9b09PmJkpkUrYR3FI9mK6A")
+		Geocode.setLanguage("en")
 		Geocode.fromLatLng(lat, lon).then(
-			response => {
-				const address = response.results[0];
-				const length = address.address_components.length;
+			(response) => {
+				const address = response.results[0]
+				const length = address.address_components.length
 				this.setState({
-					currentLocation: `${address.address_components[length - 3].long_name} ${address.address_components[length - 1].long_name} ${address.address_components[length - 2].short_name}`
-				});
+					currentLocation: `${
+						address.address_components[length - 3].long_name
+					} ${address.address_components[length - 1].long_name} ${
+						address.address_components[length - 2].short_name
+					}`,
+				})
 			},
-			error => {
-				this.setState({currentLocation: "Location not found"});
-			}
-		);
+			(error) => {
+				this.setState({ currentLocation: "Location not found" })
+			},
+		)
 	}
 
 	getLocationCoordinates(name) {
 		Geocode.fromAddress(name).then(
-			response => {
-				const coordinates = response.results[0].geometry.location;
-				this.setState({newCoordinates: coordinates});
+			(response) => {
+				const coordinates = response.results[0].geometry.location
+				this.setState({ newCoordinates: coordinates })
 			},
-			error => {
-				this.setState({newCoordinates: "City not found"});
-			}
-		);
+			(error) => {
+				this.setState({ newCoordinates: "City not found" })
+			},
+		)
 	}
 
 	async componentDidMount() {
 		await new Promise((resolve, reject) => {
 			if (navigator.geolocation) {
-				navigator.geolocation.getCurrentPosition(position => {
+				navigator.geolocation.getCurrentPosition((position) => {
 					this.setState({
 						currentCoordinates: {
 							lat: position.coords.latitude,
-							lon: position.coords.longitude
-						}
-					});
-					resolve();
-				});
+							lon: position.coords.longitude,
+						},
+					})
+					resolve()
+				})
 			}
-		});
+		})
 
 		this.fetchLocation(
 			this.state.currentCoordinates.lat,
-			this.state.currentCoordinates.lon
-		);
+			this.state.currentCoordinates.lon,
+		)
 	}
 
-	getDate = d => {
+	getDate = (d) => {
 		let months = [
 			"January",
 			"February",
@@ -106,8 +110,8 @@ class Weathers extends Component {
 			"September",
 			"October",
 			"November",
-			"December"
-		];
+			"December",
+		]
 		let days = [
 			"Sunday",
 			"Monday",
@@ -115,86 +119,85 @@ class Weathers extends Component {
 			"Wednesday",
 			"Thursday",
 			"Friday",
-			"Saturday"
-		];
+			"Saturday",
+		]
 
-		let day = days[d.getDay()];
-		let date = d.getDate();
-		let month = months[d.getMonth()];
-		let year = d.getFullYear();
+		let day = days[d.getDay()]
+		let date = d.getDate()
+		let month = months[d.getMonth()]
+		let year = d.getFullYear()
 
-		return `${day} ${date} ${month} ${year}`;
-	};
+		return `${day} ${date} ${month} ${year}`
+	}
 
 	updateTime(time) {
-		const date = new Date(time * 1000);
-		const hours = date.getHours();
-		const minutes = "0" + date.getMinutes();
+		const date = new Date(time * 1000)
+		const hours = date.getHours()
+		const minutes = "0" + date.getMinutes()
 
-		return hours + ":" + minutes.substr(-2);
+		return hours + ":" + minutes.substr(-2)
 	}
 
 	getHourOnly(time) {
-		const date = new Date(time * 1000);
-		const hours = date.getHours();
-		return hours;
+		const date = new Date(time * 1000)
+		const hours = date.getHours()
+		return hours
 	}
 
 	async fetchWithName(name) {
 		await new Promise((resolve, reject) => {
 			Geocode.fromAddress(name).then(
-				response => {
-					const coordinates = response.results[0].geometry.location;
-					this.setState({newCoordinates: coordinates});
-					resolve();
+				(response) => {
+					const coordinates = response.results[0].geometry.location
+					this.setState({ newCoordinates: coordinates })
+					resolve()
 				},
-				error => {
-					this.setState({searchStatus: "City not found"});
-				}
-			);
-		});
+				(error) => {
+					this.setState({ searchStatus: "City not found" })
+				},
+			)
+		})
 		this.fetchLocation(
 			this.state.newCoordinates.lat,
-			this.state.newCoordinates.lng
-		);
+			this.state.newCoordinates.lng,
+		)
 	}
 
 	fetchWithCoordinates(lat, lon) {
-		this.fetchLocation(lat, lon);
+		this.fetchLocation(lat, lon)
 	}
 
 	FtoC(temp) {
-		return (((temp - 32) * 5) / 9).toFixed(3);
+		return (((temp - 32) * 5) / 9).toFixed(3)
 	}
 
 	render() {
 		return (
 			<div>
-				<div className="weather-search">
+				<div className='weather-search'>
 					<Autocomplete
-						placeholder="Search by city"
-						onPlaceSelected={place => {
+						placeholder='Search by city'
+						onPlaceSelected={(place) => {
 							if (place.name === undefined) {
 								this.fetchWithCoordinates(
 									place.geometry.location.lat(),
-									place.geometry.location.lng()
-								);
+									place.geometry.location.lng(),
+								)
 							} else {
-								this.fetchWithName(place.name);
+								this.fetchWithName(place.name)
 							}
 						}}
 						types={["(regions)"]}
 					/>
 					<a
-						className="current-location"
-						href="#"
+						className='current-location'
+						href='#'
 						onClick={() =>
 							this.fetchLocation(
 								this.state.currentCoordinates.lat,
-								this.state.currentCoordinates.lon
+								this.state.currentCoordinates.lon,
 							)
-						}
-					>
+						}>
 						Use your current location
 					</a>
 				</div>
@@ -215,8 +218,8 @@ class Weathers extends Component {
 					getHourOnly={this.getHourOnly}
 				/>
 			</div>
-		);
+		)
 	}
 }
 
-export default Weathers;
+export default Weathers
